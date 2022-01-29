6d1b80f2d28d8f20fe4b8495c1f941ba47e59be8
Bharath Kumar <kumar7bharath@gmail.com>
2022-01-29 14:50:08 -0500

    change fov, pipeling changes

--- pick_and_place/scripts/ggcnn_pipeline.py
+++ pick_and_place/scripts/ggcnn_pipeline.py
@@ -22,19 +22,24 @@ def debug():
 def process_and_execute(req):
     global pick_and_place
 
+    rospy.loginfo("waiting for service: ggcnn_service/predict")
     rospy.wait_for_service("ggcnn_service/predict")
+    rospy.loginfo("Service call successful")
+
     srv_handle = rospy.ServiceProxy("ggcnn_service/predict", GraspPrediction)
     response = srv_handle()
+    rospy.loginfo("response: " + str(response))
     x = response.best_grasp.pose.position.x
     y = response.best_grasp.pose.position.y
     z = response.best_grasp.pose.position.z 
     (rx, ry, rz) = euler_from_quaternion([response.best_grasp.pose.orientation.w, response.best_grasp.pose.orientation.x, response.best_grasp.pose.orientation.y, response.best_grasp.pose.orientation.z])
 
-    pick_and_place.setPickPose(x, y, z, rx, ry, rz)
+    pick_and_place.setPickPose(x, y, z + 0.02, rx, ry, rz)
     pick_and_place.setDropPose(0.0, 0.4, 0.5, 0, pi, 0)
     pick_and_place.setGripperPose(0.01, 0.01)
     
-    pick_and_place.execute_pick_and_place()
+    # pick_and_place.execute_pick_and_place()
+    pick_and_place.execute_cartesian_pick_and_place()
 
     return True
 
